fcf1f57a9c6859f3ab67ea4c59248125c0ffa34d
RouvenReuter91 <rouven.reuter@viscopic.de>
2020-08-15 15:36:03 +0200

    TODO: Edit repo in xml string
    Cleaned up code

--- jenkinsConfig.py
+++ jenkinsConfig.py
@@ -1,17 +1,10 @@
 import jenkins
+import sys
+
+#arguments: 1->Loginname 2->password 3->jobname 4->repoadress
 
 config = open("config.xml", "r")
 configAsString = config.read()
 
-
-server = jenkins.Jenkins('http://104.248.16.120:8080', username='devops', password='admin123')
-server.create_job("Test2", configAsString)
-
-
-
-jobs = server.get_jobs()
-print(jobs)
-
-#myconfig = server.get_job_config("Test")
-
-#print(myconfig)
+server = jenkins.Jenkins('http://104.248.16.120:8080', username=sys.argv[1], password=sys.argv[2])
+server.create_job(sys.argv[3], configAsString)
