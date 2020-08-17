fbab86e3fbeb7b71bdd77f975300659f06b450cd
RouvenReuter91 <rouven.reuter@viscopic.de>
2020-08-17 20:38:06 +0200

    [jenkinsConfig.py] refactored code

--- jenkinsConfig.py
+++ jenkinsConfig.py
@@ -2,17 +2,16 @@ import jenkins
 import sys
 import socket
 import netifaces as ni
-ni.ifaddresses('eth0')
-ip = ni.ifaddresses('eth0')[ni.AF_INET][0]['addr']
-print("adress one")
-print(ip)  # should print "192.168.100.37"
+
 
 
 
 #arguments: 1->Loginname 2->password 3->Jobname 4->repoadress
 
-ip_address = socket.gethostbyname(socket.gethostname())
-print(f"IP Address: {ip_address}")
+ni.ifaddresses('eth0')
+ip = ni.ifaddresses('eth0')[ni.AF_INET][0]['addr']
+print("adress one")
+print(ip)  # should print "192.168.100.37"
 
 for value in sys.argv:
     print(value)
@@ -22,5 +21,5 @@ configAsString = config.read()
 
 configAsString = configAsString.replace("#Repo", sys.argv[4])
 
-server = jenkins.Jenkins(f'http://{ip_address}:8080', username=sys.argv[1], password=sys.argv[2])
+server = jenkins.Jenkins(f'http://{ip}:8080', username=sys.argv[1], password=sys.argv[2])
 server.create_job(sys.argv[3], configAsString)
