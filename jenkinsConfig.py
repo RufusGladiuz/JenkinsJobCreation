b25dda103405c4d3668bfc85b19161c6061c61af
RouvenReuter91 <rouven.reuter@viscopic.de>
2020-08-17 18:55:14 +0200

    Added debug line

--- jenkinsConfig.py
+++ jenkinsConfig.py
@@ -7,10 +7,13 @@ import socket
 ip_address = socket.gethostbyname(socket.gethostname())
 print(f"IP Address: {ip_address}")
 
+for value in sys.argv:
+    print(value)
+
 config = open("config.xml", "r")
 configAsString = config.read()
 
-configAsString = configAsString.replace("Repo", sys.argv[4])
+configAsString = configAsString.replace("#Repo", sys.argv[4])
 
 server = jenkins.Jenkins(f'http://{ip_address}:8080', username=sys.argv[1], password=sys.argv[2])
 server.create_job(sys.argv[3], configAsString)
