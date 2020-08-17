50f0b0b5c817b95498e760a35499096dafdd98f5
RouvenReuter91 <rouven.reuter@viscopic.de>
2020-08-17 22:19:20 +0200

    Get https://www.digitalocean.com/community/tools/python-digitalocean to get server ip and create jenkins job

--- jenkinsConfig.py
+++ jenkinsConfig.py
@@ -3,15 +3,12 @@ import sys
 import socket
 import netifaces as ni
 
-
-
-
 #arguments: 1->Loginname 2->password 3->Jobname 4->repoadress
 
-ni.ifaddresses('eth0')
-ip = ni.ifaddresses('eth0')[ni.AF_INET][0]['addr']
-print("adress one")
-print(ip)  # should print "192.168.100.37"
+# ni.ifaddresses('eth0')
+# ip = ni.ifaddresses('eth0')[ni.AF_INET][0]['addr']
+# print("adress one")
+# print(ip)  # should print "192.168.100.37"
 
 for value in sys.argv:
     print(value)
@@ -21,5 +18,5 @@ configAsString = config.read()
 
 configAsString = configAsString.replace("#Repo", sys.argv[4])
 
-server = jenkins.Jenkins(f'http://{ip}:8080', username=sys.argv[1], password=sys.argv[2])
+server = jenkins.Jenkins(f'http://178.128.194.93:8080', username=sys.argv[1], password=sys.argv[2])
 server.create_job(sys.argv[3], configAsString)
